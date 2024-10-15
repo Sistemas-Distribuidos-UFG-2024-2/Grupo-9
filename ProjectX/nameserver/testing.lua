e29e5314c3d7cabd09581764fe0c9a44829ebc23
Gabriel Miranda <gabriel.miranda@discente.ufg.br>
2024-10-15 00:11:03 -0300

    WIP: trabalhando nos testes

--- ProjectX/nameserver/testing.lua
+++ ProjectX/nameserver/testing.lua
@@ -4,61 +4,79 @@
 --- Nameserver testing script
 ---
 return {
+
+new_testing = function ()
     local socket = require("socket")
 
     local namecache_host = "localhost"
     local namecache_port = 3000
 
-    local client_get = assert(socket.udp())
-    local client_delete = assert(socket.udp())
-
     local coroutines = {}
 
-    function receive (client)
+    local function receive (client)
         client:timeout(0)
         local s, status = client:receive()
         if status == "timeout" then
-            coroutine.yeld(client)
+            coroutine.yield(client)
         end
         return s, status
     end
 
-    function post_request (host, port, name, ip)
+    local function post_request (host, port, name, ip)
         local client_post = assert(socket.udp())
+        local data = "response: "
         client_post:sendto("POST /"..name.."?ip="..ip, host, port)
         while true do
             local s, status = receive(client_post)
+            if s ~= nil then
+                data = data..s
+            end
             if status == "closed" then break end
         end
         client_post:close()
-        print("Posted: "..name.." - "..ip.."\nReceived: "..s)
+        print("Posted: "..name.." - "..ip.."\nReceived: "..data)
     end
 
-    function get_request (host, post, name)
+    local function get_request (host, port, name)
         local client_get = assert(socket.udp())
+        local data = "response: "
         client_get:sendto("GET /"..name, host, port)
         while true do
             local s, status = receive(client_get)
+            if s ~= nil then
+                data = data..s
+            end
             if status == "closed" then break end
         end
         client_get:close()
-        print("Get'd: "..name.." - "..ip.."\nReceived: "..s)
+        print("Get'd: "..name.."\nReceived: "..data)
     end
 
-    function delete_request (host, port, name, ip)
+    local function delete_request (host, port, name, ip)
         local client_delete = assert(socket.udp())
+        local data = "response: "
         client_delete:sendto("DELETE /"..name.."?ip="..ip, host, port)
         while true do
             local s, status = receive(client_delete)
+            if s ~= nil then
+                data = data..s
+            end
             if status == "closed" then break end
         end
         client_delete:close()
-        print("Deleted: "..name.." - "..ip.."\nReceived: "..s)
+        print("Deleted: "..name.." - "..ip.."\nReceived: "..data)
     end
 
-    function create_test(ips,host,port,names)
-        local co = coroutine.create(function()
-            )
+    local function create_test(ips,host,port,names)
+        for i=1, #names do
+            for j=1, #ips do
+                local co = coroutine.create(function()
+                    post_request(host, port, names[i], ips[j])
+                end
+                )
+            end
+        end
     end
+end
 }
 
