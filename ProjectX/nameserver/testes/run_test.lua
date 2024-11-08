9c6d79c23f109c8828a5da28ed02b2bf6cf7c2cb
Gabriel Miranda <gabriel.miranda@discente.ufg.br>
2024-11-08 19:04:36 -0300

    Fix: alterando local dos testes e mudando uma variável no script de teste.

--- ProjectX/nameserver/testes/run_test.lua
+++ ProjectX/nameserver/testes/run_test.lua
@@ -4,15 +4,15 @@ local testing = require("testing")
 
 local ip = "127.0.0.1"
 local port = 3000
-local n = 30
-local m = 10
+local ip_quant = 30
+local name_quant = 10
 local names = {}
 local ips = {}
-for i=1,n do
+for i=1,ip_quant do
     ips[i] = "192.168.0."..i
 end
 
-for i=1,m do
+for i=1,name_quant do
     names[i] = "teste"..i
 end
 
